24f0bfb246be090bd95c1fca957ee38690964ba9
nroshdy92 <156032373+nroshdy92@users.noreply.github.com>
2024-01-19 19:13:59 -0500

    Update exam.tf

--- exam.tf
+++ exam.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
-  location = "Morocco"
+  location = "canadaeast"
 }
 
 resource "azurerm_storage_account" "example" {
