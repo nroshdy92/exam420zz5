5612d8b832a830c3f8fdb53875b91127aaf6e49b
nroshdy92 <156032373+nroshdy92@users.noreply.github.com>
2024-01-19 19:55:37 -0500

    Update exam.tf

--- exam.tf
+++ exam.tf
@@ -1,59 +1,42 @@
-"provider" "azurerm" {"features"} {}
+windowsfunctionapp:
+  - name: examplewindowsfunctionapp
+    account_tier: Standard
+    account_replication_type: lrs
+    os_type: Windows
+    sku_name: y1
 
-"resource" "azurerm_resource_group" "example" {
-  "location" = "West Europe"
+provider: 
+  azurerm: 
+    features: {}
 
-  "name" = "example-resources"
-}
+resource:
+  azurerm_resource_group:
+    example:
+      name: "example-resources"
+      location: "West Europe"
 
-"resource" "azurerm_service_plan" "example" {
-  "location" = "${azurerm_resource_group.example.location}"
+  azurerm_storage_account:
+    example:
+      name: "windowsfunctionappsa"
+      resource_group_name: "${azurerm_resource_group.example.name}"
+      location: "${azurerm_resource_group.example.location}"
+      account_tier: "Standard"
+      account_replication_type: "LRS"
 
-  "name" = "example-app-service-plan"
+  azurerm_service_plan:
+    example:
+      name: "example-app-service-plan"
+      resource_group_name: "${azurerm_resource_group.example.name}"
+      location: "${azurerm_resource_group.example.location}"
+      os_type: "Windows"
+      sku_name: "Y1"
 
-  "os_type" = "Windows"
-
-  "resource_group_name" = "${azurerm_resource_group.example.name}"
-
-  "sku_name" = "Y1"
-}
-
-"resource" "azurerm_storage_account" "example" {
-  "account_replication_type" = "LRS"
-
-  "account_tier" = "Standard"
-
-  "location" = "${azurerm_resource_group.example.location}"
-
-  "name" = "windowsfunctionappsa"
-
-  "resource_group_name" = "${azurerm_resource_group.example.name}"
-}
-
-"resource" "azurerm_windows_function_app" "example" {
-  "location" = "${azurerm_resource_group.example.location}"
-
-  "name" = "example-windows-function-app"
-
-  "resource_group_name" = "${azurerm_resource_group.example.name}"
-
-  "service_plan_id" = "${azurerm_service_plan.example.id}"
-
-  "site_config" = {}
-
-  "storage_account_access_key" = "${azurerm_storage_account.example.primary_access_key}"
-
-  "storage_account_name" = "${azurerm_storage_account.example.name}"
-}
-
-"windowsfunctionapp" = {
-  "account_replication_type" = "lrs"
-
-  "account_tier" = "Standard"
-
-  "name" = "examplewindowsfunctionapp"
-
-  "os_type" = "Windows"
-
-  "sku_name" = "y1"
-}
+  azurerm_windows_function_app:
+    example:
+      name: "example-windows-function-app"
+      resource_group_name: "${azurerm_resource_group.example.name}"
+      location: "${azurerm_resource_group.example.location}"
+      storage_account_name: "${azurerm_storage_account.example.name}"
+      storage_account_access_key: "${azurerm_storage_account.example.primary_access_key}"
+      service_plan_id: "${azurerm_service_plan.example.id}"
+      site_config: {}
